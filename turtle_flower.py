a0592d856a50625600326d2ce98f29dd33f32436
gimmedaangel <romanpryym@gmail.com>
2025-09-13 14:52:40 +0100

    Update

--- turtle_flower.py
+++ turtle_flower.py
@@ -1,20 +1,14 @@
 import turtle
+import math
 t = turtle.Turtle()
 t.color("red", "orange")
 t.begin_fill()
 t.speed(400)
-<<<<<<< HEAD
-for i in range(800):
+for i in range(300):
 
-    t.fd(140)
+    t.fd(5 * 10)
     t.rt(175)
-=======
-for i in range(500):
 
-    t.fd(140)
-    t.rt(179)
->>>>>>> ecf9585c2019de32f80b4759eb6231267a0cf705
-    t.fd(140)
+
 t.end_fill()
-
 turtle.done()
